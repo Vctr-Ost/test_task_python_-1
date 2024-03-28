9af9d864e743e17dce5c08c4aa956d5e177e7e99
Viktor <ostapenko.vctr@gmail.com>
2024-03-28 00:59:54 +0000

    Add scheduler and auto_yesterday_dt

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import schedule
+import time
+import datetime
 from api_usage import installs, costs, events, orders
 
-dt = {"date": "2024-03-25"}
 
-installs.installs_fn(dt)
-costs.costs_fn(dt)
-orders.orders_fn(dt)
-events.events_fn(dt)
+def daily_run():
+    yesterday_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    dt = {"date": yesterday_str}
+
+    print(dt)
+
+    installs.installs_fn(dt)
+    costs.costs_fn(dt)
+    orders.orders_fn(dt)
+    events.events_fn(dt)
+
+
+def main():
+
+    schedule.every().day.at('00:58').do(daily_run)      #  -2 hours (UTC)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
